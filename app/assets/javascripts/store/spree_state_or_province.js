7e25a84ae4ae20e2af7e941c0cb0510ded2ade53
Yonah Forst <joshblour@hotmail.com>
2014-02-04 22:08:53 +0100

    fix js

--- app/assets/javascripts/store/spree_state_or_province.js
+++ app/assets/javascripts/store/spree_state_or_province.js
@@ -2,28 +2,29 @@
 Spree.ready(function($) {
   var getCountryId, updateStateAndZipName, setStateName, setZipName;
   
+  if (Spree.state_and_zip_titles != undefined) {
+  
     getCountryId = function(region) {
       return $('#' + region + 'country select').val();
     };
     
-    if (Spree.state_and_zip_titles != undefined) {
     
-      updateStateAndZipName = function(region) {
-        var countryId;
-        var country_hash
-        countryId = parseInt(getCountryId(region));
-        country_hash = Spree.state_and_zip_titles[countryId];
-        default_titles = Spree.default_titles;
-      
-        if (country_hash != null) {
-            setStateName(country_hash.state_title, region)
-            setZipName(country_hash.zip_title, region)
-          } else {
-            setStateName(default_titles.state_title, region)
-            setZipName(default_titles.zip_title, region)
-          }
-      };
-    }
+    updateStateAndZipName = function(region) {
+      var countryId;
+      var country_hash
+      countryId = parseInt(getCountryId(region));
+      country_hash = Spree.state_and_zip_titles[countryId];
+      default_titles = Spree.default_titles;
+    
+      if (country_hash != null) {
+          setStateName(country_hash.state_title, region)
+          setZipName(country_hash.zip_title, region)
+        } else {
+          setStateName(default_titles.state_title, region)
+          setZipName(default_titles.zip_title, region)
+        }
+    };
+
     
     setStateName = function(new_name, region) {
       $('#' + region + 'state label').html(new_name)
@@ -44,5 +45,5 @@ Spree.ready(function($) {
 
     updateStateAndZipName('b');
 
-
+  }
 });
